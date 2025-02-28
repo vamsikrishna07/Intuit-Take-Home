6aa1de79b4ed0c8394b65cc60cb4b161adeb7c50
Vamsi Krishna Kanagala <vamsikrishnakanagala@mac.mynetworksettings.com>
2025-02-27 23:34:46 -0500

    Minor Bug Fix

--- MessageDecoder.py
+++ MessageDecoder.py
@@ -3,7 +3,7 @@ class MessageDecoder:
     def decode(self, matrix, index):
         for row in matrix:
             print(row)
-            matrix = np.array(matrix)
-            filename = "output-"+str(index)+".txt"
-            np.savetxt(filename, matrix, fmt='%s', delimiter=',')
-            print(f"Message saved to {filename}")
+        matrix = np.array(matrix)
+        filename = "output-"+str(index)+".txt"
+        np.savetxt(filename, matrix, fmt='%s', delimiter=',')
+        print(f"Message saved to {filename}")
